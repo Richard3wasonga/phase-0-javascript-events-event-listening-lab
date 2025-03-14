3e2563e380d3cf872d8f8d9574d16fc1d9a6b67e
richard <richard.wasonga@student.moringaschool.com>
2025-03-14 18:29:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+let input = document.getElementById('button');
+function addingEventListener(){
+    let but = document.getElementsByTagName("input")
+    for(let btn of but){
+        btn.addEventListener('click', input)
+    }
 }
+
